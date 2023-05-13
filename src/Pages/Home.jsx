8f279cbf56dd4df4a57e673c6b2a0f1f778218f5
Wilson Ayoola <ayoolawilson771@gmail.com>
2023-05-13 12:38:25 +0100

    Added an image

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,5 @@
 import  './Home.css'
+import BookShelf from './Assets/Bookshelf.svg'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function Home() {
@@ -9,8 +10,26 @@ function Home() {
             <div className='button'>
                 <button className='btn btn-outline-primary'>Get Started</button>
             </div>
+            <img src={BookShelf} className="bookshelf-react" alt="logo" />
+            <h2> We Will ....</h2>
+            <ol className='list'>
+                <li className='list-item'>
+                    Store your books for you
+                </li>
+                <li className='list-item'>
+                    Restore them if damaged
+                </li>
+                <li className='list-item'>
+                    We'll also do the same for E-books
+                </li>
+                <li className='list-item'>
+                    Lorem ipsum innit??
+                </li>
+                <li className='list-item'>
+                    Oloun we outchea
+                </li>
+            </ol>
         </>
-        
     )
 }
 
