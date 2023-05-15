b05a52b735bab7d20c4d5f7e06e9eed24ebf671a
Wilson Ayoola <ayoolawilson771@gmail.com>
2023-05-15 16:23:56 +0100

    Finished the home page and started another page

--- src/Pages/Book.jsx
+++ src/Pages/Book.jsx
@@ -1,6 +1,21 @@
+import './Book.css'
+import Pride from './Assets/Pride&Prejudice.svg'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
 function Book() {
     return(
-        <h1>Book</h1>
+        <>
+        <div className='head'>
+            <h1>Book</h1>
+        {/* <div className="card" style="width: 18rem;">
+            <img src={Pride} className="p&p" alt="logo" />
+                <div className="card-body">
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div> */}
+        </div>
+        </>
     )
 }
 
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,34 +1,59 @@
 import  './Home.css'
-import BookShelf from './Assets/Bookshelf.svg'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import BookShelf from './Assets/Bookshelf.svg'
 
 function Home() {
     return(
         <>
+        <div className='head'>
             <h1 className='header'>Your Personal Book Manager</h1>
-            <p>Lorem Ipsum dolor sit amet conseceteur muniz adipiscing</p>
-            <div className='button'>
-                <button className='btn btn-outline-primary'>Get Started</button>
-            </div>
-            <img src={BookShelf} className="bookshelf-react" alt="logo" />
-            <h2> We Will ....</h2>
-            <ol className='list'>
-                <li className='list-item'>
-                    Store your books for you
-                </li>
-                <li className='list-item'>
-                    Restore them if damaged
-                </li>
-                <li className='list-item'>
-                    We'll also do the same for E-books
-                </li>
-                <li className='list-item'>
-                    Lorem ipsum innit??
-                </li>
-                <li className='list-item'>
-                    Oloun we outchea
-                </li>
-            </ol>
+                <p>Lorem Ipsum dolor sit amet conseceteur muniz adipiscing</p>
+                <div className='button'>
+                    <button className='btn btn-outline-primary'>Get Started</button>
+                </div>
+                <img src={BookShelf} className="bookshelf-react" alt="logo" />
+                <h2> We Will ....</h2>
+                <ol className='list'>
+                    <li className='list-item'>
+                        Store your books for you
+                    </li>
+                    <li className='list-item'>
+                        Restore them if damaged
+                    </li>
+                    <li className='list-item'>
+                        We'll also do the same for E-books
+                    </li>
+                    <li className='list-item'>
+                        Lorem ipsum
+                    </li>
+                    <li className='list-item'>
+                        Dolor sit amet, consectetur adipiscing elit
+                    </li>
+                    <li className='list-item'>
+                        Dolor sit amet, consectetur adipiscing elit
+                    </li>
+                    <li className='list-item'>
+                        Dolor sit amet, consectetur adipiscing elit
+                    </li>
+                    <li className='list-item'>
+                        Dolor sit amet, consectetur adipiscing elit
+                    </li>
+                    <li className='list-item'>
+                        Dolor sit amet, consectetur adipiscing elit
+                    </li>
+                    <li className='list-item'>
+                        Dolor sit amet, consectetur adipiscing elit
+                    </li>
+                </ol>
+        </div>
+        <div className='footer'>
+            <h1>
+                So let us work for you
+            </h1>
+            {/* <p className='footer-text'>
+                Words
+            </p> */}
+        </div>
         </>
     )
 }
