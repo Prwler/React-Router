2eab77e9208a45ab7814bce9b032edcb02a1dd57
Wilson Ayoola <ayoolawilson771@gmail.com>
2023-05-16 15:06:14 +0100

    Final commit. Until further development

--- src/Pages/Book.jsx
+++ src/Pages/Book.jsx
@@ -8,12 +8,6 @@ function Book() {
         <>
         <div className='head'>
             <h1>Book</h1>
-        {/* <div className="card" style="width: 18rem;">
-            <img src={Pride} className="p&p" alt="logo" />
-                <div className="card-body">
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                </div>
-            </div> */}
         </div>
         </>
     )
